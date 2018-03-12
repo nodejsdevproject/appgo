7bd54fc5a64e351e82639f79fe99d37265efeaa9
DESKTOP-NMIQAAP\FuWei <FuWei007@gmail.com>
2018-03-12 09:35:21 +0800

    bind menu

--- webadmin/src/App.js
+++ webadmin/src/App.js
@@ -10,7 +10,7 @@ import './helper'
 import { translateObject } from './helper';
 import { translateEntitySchema } from './helper';
 
-import 'antd/dist/antd.css';  
+import 'antd/dist/antd.css';
 import { Layout, Menu, Breadcrumb, Icon } from 'antd';
 const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
@@ -51,9 +51,9 @@ class App extends Component {
 
         // Use the default lanuage to render the page. If you pick another lanuage. you can call the follow line to change it.
         this.setState({
-          uiModel:{
-            appConfig : this.getTranslatedAppConfig(this.state.uiModel.appConfig.defaultLanguage),
-            entitySchema : this.getTranslatedEntitySchema(this.state.uiModel.appConfig.defaultLanguage),
+          uiModel: {
+            appConfig: this.getTranslatedAppConfig(this.state.uiModel.appConfig.defaultLanguage),
+            entitySchema: this.getTranslatedEntitySchema(this.state.uiModel.appConfig.defaultLanguage),
           },
         });
         // Translate the date from local.Why? We need the ability to dynamic translate. So if you change the lanauge later. 
@@ -79,7 +79,21 @@ class App extends Component {
     return translateEntitySchema(this.state.uiModel.LocalizedStrings, lang, this.state.uiModel.entitySchema);
   }
 
-
+  recursion(dataSource) {
+    return (
+      dataSource.map((menu, index) => {
+        if (menu.children) {
+          return (
+            <SubMenu key={menu.key} title={menu.title}>
+              {this.recursion(menu.children)}
+            </SubMenu>
+          )
+        } else {
+          return (<Menu.Item key={menu.key}>{menu.title}</Menu.Item>)
+        }
+      })
+    )
+  }
 
   render() {
     // We get everything then start render. Idealy each compoenent shoud have it own config/model
@@ -97,59 +111,44 @@ class App extends Component {
 
 
         <Layout>
-        <Header className="header">
-          <div className="logo" />
-          <Menu
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={['2']}
-            style={{ lineHeight: '64px' }}
-          >
-            <Menu.Item key="1">nav 1</Menu.Item>
-            <Menu.Item key="2">nav 2</Menu.Item>
-            <Menu.Item key="3">nav 3</Menu.Item>
-          </Menu>
-        </Header>
-        <Layout>
-          <Sider width={200} style={{ background: '#fff' }}>
+          <Header className="header">
+            <div className="logo" />
             <Menu
-              mode="inline"
-              defaultSelectedKeys={['1']}
-              defaultOpenKeys={['sub1']}
-              style={{ height: '100%', borderRight: 0 }}
+              theme="dark"
+              mode="horizontal"
+              defaultSelectedKeys={['2']}
+              style={{ lineHeight: '64px' }}
             >
-              <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
-                <Menu.Item key="1">option1</Menu.Item>
-                <Menu.Item key="2">option2</Menu.Item>
-                <Menu.Item key="3">option3</Menu.Item>
-                <Menu.Item key="4">option4</Menu.Item>
-              </SubMenu>
-              <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
-                <Menu.Item key="5">option5</Menu.Item>
-                <Menu.Item key="6">option6</Menu.Item>
-                <Menu.Item key="7">option7</Menu.Item>
-                <Menu.Item key="8">option8</Menu.Item>
-              </SubMenu>
-              <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
-                <Menu.Item key="9">option9</Menu.Item>
-                <Menu.Item key="10">option10</Menu.Item>
-                <Menu.Item key="11">option11</Menu.Item>
-                <Menu.Item key="12">option12</Menu.Item>
-              </SubMenu>
+              {
+                this.recursion(this.state.uiModel.appConfig.headerMenu.items)
+              }
             </Menu>
-          </Sider>
-          <Layout style={{ padding: '0 24px 24px' }}>
-            <Breadcrumb style={{ margin: '16px 0' }}>
-              {/* <Breadcrumb.Item>Home</Breadcrumb.Item>
-              <Breadcrumb.Item>List</Breadcrumb.Item>
-              <Breadcrumb.Item>App</Breadcrumb.Item> */}
-            </Breadcrumb>
-            <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
-              Content
+          </Header>
+          <Layout>
+            <Sider width={200} style={{ background: '#fff' }}>
+              <Menu
+                mode="inline"
+                defaultSelectedKeys={['1']}
+                defaultOpenKeys={['sub1']}
+                style={{ height: '100%', borderRight: 0 }}
+              >
+               {
+                this.recursion(this.state.uiModel.appConfig.leftMenu.items)
+              }
+              </Menu>
+            </Sider>
+            <Layout style={{ padding: '0 24px 24px' }}>
+              <Breadcrumb style={{ margin: '16px 0' }}>
+                <Breadcrumb.Item>Home</Breadcrumb.Item>
+                <Breadcrumb.Item>List</Breadcrumb.Item>
+                <Breadcrumb.Item>App</Breadcrumb.Item>
+              </Breadcrumb>
+              <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
+                Content
             </Content>
+            </Layout>
           </Layout>
         </Layout>
-      </Layout>
       );
     }
     return <div className="App-loader"></div>;
